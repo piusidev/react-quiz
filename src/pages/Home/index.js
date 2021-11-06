65b1d793591d3358869326de75cd5036728e11fe
Leonardo Piusi <leopidev@gmail.com>
2021-11-06 11:27:40 -0300

    refactor: homepage set game config

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,30 +8,30 @@ import { ButtonsContainer, Homepage, Wrapper } from "./styles"
 
 const Home = () => {
   const { config } = useContext(GameContext)
-  const quantityRef = useRef({})
-  const difficultyRef = useRef({})
+  const quantityRef = useRef([])
+  const difficultyRef = useRef([])
   const history = useHistory()
 
   const startGame = () => {
     let quantity = quantityRef.current
     let difficulty = difficultyRef.current
 
-    Object.entries(quantity).map(([key, value]) => {
-      if(value.checked === true) {
-        quantity = value.value
+    quantity.map((item) => {
+      if(item.checked === true) {
+        quantity = item.value
       }
     })
 
-    Object.entries(difficulty).map(([key, value]) => {
-      if(value.checked === true) {
-        difficulty = value.value
+    difficulty.map((item) => {
+      if(item.checked === true) {
+        difficulty = item.value
       }
     })
 
     config.quantity = quantity
     config.difficulty = difficulty
 
-    history.push('/quizz')
+    history.push(`/quizz?quantity=${quantity}&difficulty=${difficulty}`)
   }
   
   return(
@@ -47,10 +47,10 @@ const Home = () => {
                 Quantas perguntas quer responder?
               </Typography>
               <ButtonsContainer>
-                <Option ref={el => quantityRef.current['0'] = el} text={'5'} value={5} name={'quantityQuestions'} />
-                <Option ref={el => quantityRef.current['1'] = el}  text={'10'} value={10} name={'quantityQuestions'} />
-                <Option ref={el => quantityRef.current['2'] = el}  text={'15'} value={15} name={'quantityQuestions'} />
-                <Option ref={el => quantityRef.current['3'] = el}  text={'20'} value={20} name={'quantityQuestions'} />
+                <Option ref={el => quantityRef.current.push(el)} text={'5'}  value={5} name={'quantityQuestions'} />
+                <Option ref={el => quantityRef.current.push(el)} text={'10'} value={10} name={'quantityQuestions'} />
+                <Option ref={el => quantityRef.current.push(el)} text={'15'} value={15} name={'quantityQuestions'} />
+                <Option ref={el => quantityRef.current.push(el)} text={'20'} value={20} name={'quantityQuestions'} />
               </ButtonsContainer>
             </div>
             <div>
@@ -58,9 +58,9 @@ const Home = () => {
                 Em qual dificuldade quer jogar?
               </Typography>
               <ButtonsContainer>
-                <Option ref={el => difficultyRef.current['0'] = el} text={'Fácil'} value={'easy'} name={'difficultyQuestions'} />
-                <Option ref={el => difficultyRef.current['1'] = el} text={'Médio'} value={'medium'} name={'difficultyQuestions'} />
-                <Option ref={el => difficultyRef.current['2'] = el} text={'Díficil'} value={'hard'} name={'difficultyQuestions'} />
+                <Option ref={el => difficultyRef.current.push(el)} text={'Fácil'} value={'easy'} name={'difficultyQuestions'} />
+                <Option ref={el => difficultyRef.current.push(el)} text={'Médio'} value={'medium'} name={'difficultyQuestions'} />
+                <Option ref={el => difficultyRef.current.push(el)} text={'Díficil'} value={'hard'} name={'difficultyQuestions'} />
               </ButtonsContainer>
             </div>
           </section>
